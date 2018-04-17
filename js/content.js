e786f9317417db54d3ae3db48a5d650d9f1ce7fa
Tracy Spitler <tracyspitler@gmail.com>
2018-04-16 23:39:43 -0400

    Updated css and html for content.html and index.html

--- js/content.js
+++ js/content.js
@@ -1,32 +1,33 @@
 /* JS Fetch
 ================================================== */
+let ipsums = 'https://api.myjson.com/bins/uf82v';
 
-const client_id = 'client_id=f852db6eb81775d1c6d04696094453c7228fa15b76122906fdfaf31ad4397c62';
-const API = 'https://api.unsplash.com/';
-let ipsums = '../ipsums.json';
+let randomPhoto = API + 'search/photos/?page=3&query=travel&' + client_id;
 
 fetch(ipsums)
   .then(response => response.json())
   .then(data => {
+    // Here's a list of unsplash photos!
+    console.log(data)
 
-    for (var i = 0; data.ipsum.length; i++) {
+    for (var i = 0; i < 9; i++) {
 
       // Fill Gallery
-      var post = '<section id="ipsum-gallery" class="container mb-5">';
+      var post = '<p>Please let this work!</p>';
 
-      for (i = 0; i < 9; i++) {
-        post += '<div id="ipsum" class="row justify-content-center">';
-        post += '<article class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-5">';
-        post += '<div class="bg-container">';
-        post += '<p class="description">' + data.ipsum[i].description + '</p>';
-        post += '<div class="overlay">';
-        post += '<h5 class="">' + data.ipsum[i].name + '</h5>';
-        post += '<p class="example">' + data.ipsum[i].example + '</p>';
-        post += '<p><a class="info" href="' + data.ipsum[i].url + '">Get it!</a></p>';
-        post += '</div></div></article>';
-        post += '</div></section>';
-      }
+      // for (i = 0; i < 9; i++) {
+      //   post += '<article class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-5">';
+      //   post += '<div class="bg-container">';
+      //   post += '<p class="description">' + data.ipsum[i].description + '</p>';
+      //   post += '<div class="overlay">';
+      //   post += '<h5 class="">' + data.ipsum[i].name + '</h5>';
+      //   post += '<p class="example">' + data.ipsum[i].example + '</p>';
+      //   post += '<p><a class="info" href="' + data.ipsum[i].url + '">Get it!</a></p>';
+      //   post += '</div></div></article>';
+      // }
 
-      document.querySelector('#ipsum-gallery').innerHTML = post;
+
+      //post += '</ul>';
+      document.querySelector('#ipsum').innerHTML = post;
     }
   });
